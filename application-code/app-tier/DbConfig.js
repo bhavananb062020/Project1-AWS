de5eb28bb3946192b013144df608bf57127b8fc9
bhavananb062020 <bhavananb2003@gmail.com>
2026-09-18 13:39:02 +0530

    changes in DBconfig

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-1.c380a08uukyc.ap-south-1.rds.amazonaws.com',
+    DB_HOST : 'database-1.c52oqqswaikc.ap-south-1.rds.amazonaws.com',
     DB_USER : 'admin',
-    DB_PWD : 'kastro2025',
+    DB_PWD : 'Bhanu123',
     DB_DATABASE : 'webappdb'
 });
