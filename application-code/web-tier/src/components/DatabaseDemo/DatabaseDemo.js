0eaa01d4183b34cc52919293fc15e498a464222a
bhavananb062020 <bhavananb2003@gmail.com>
2026-09-18 14:08:34 +0530

    Changed the backend alias url

--- application-code/web-tier/src/components/DatabaseDemo/DatabaseDemo.js
+++ application-code/web-tier/src/components/DatabaseDemo/DatabaseDemo.js
@@ -21,7 +21,7 @@
           }
 
         populateData(){
-            this.fetch_retry('http://43.205.94.181:4000/transaction',3)
+            this.fetch_retry('http://api.bhavanaproject.shop:4000/transaction',3)
             .then(res => res.json())
             .then((data) => {
               this.setState({ transactions : data.result });
@@ -59,7 +59,7 @@
            const requestOptions = {
                method: 'DELETE'
            }
-           fetch('http://43.205.94.181:4000/transaction', requestOptions)
+           fetch('http://api.bhavanaproject.shop:4000/transaction', requestOptions)
            .then(response => response.json())
            .then(data => this.populateData())
 
@@ -76,7 +76,7 @@
                 body: JSON.stringify({"amount":this.state.text_amt, "desc" :this.state.text_desc})
             }
             
-            fetch('http://43.205.94.181:4000/transaction', requestOptions)
+            fetch('http://api.bhavanaproject.shop:4000/transaction', requestOptions)
             .then(response => response.json())
             .then(data => this.populateData())
             
